5236a9b0ed7830113b308c64f912c28c21571eb4
Wei Han <xingheng907@hotmail.com>
2015-01-14 13:56:11 +0800

    Initial commit.

---
+++ ReadingTogether/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReadingTogether
+//
+//  Created by WeiHan on 1/14/15.
+//  Copyright (c) 2015 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
